28517224cdd6f38b20ccc8b96b5e8f66c33d08fa
Barlock42 <60748692+Barlock42@users.noreply.github.com>
2022-12-20 23:14:40 +0900

    Styles and AuthModal
    Added styles and AuthModal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,26 @@
 const Home = () => {
-    return <div>Home</div>
-}
+  const authToken = true;
 
-export default Home
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div className="overlay">
+      <div className="home">
+        <h1>
+          Welcome to our Tinder app! We're glad you're here. We hope you'll find
+          someone special to connect with. Our app is designed to make it easy
+          for you to discover new people who share your interests and values.
+          Just swipe right to like someone, and swipe left to pass. You never
+          know who you might find!
+        </h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Sign out" : "Sign up"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
